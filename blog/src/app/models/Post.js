6fb6ce7904f17dc2a94c712ae98bad45785ef878
nguyencongminh-dev <nguyencongminh.official@gmail.com>
2021-10-01 21:54:16 +0700

    ok

--- blog/src/app/models/Post.js
+++ blog/src/app/models/Post.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+var slug = require('mongoose-slug-generator')
+
+mongoose.plugin(slug);
 
   const posts = new Schema({
     name: { type: String, maxlength: 255 },
     description: { type: String },
     image: { type: String },
-    slug: {type: String, maxlength: 255}
+    slug: {type: String, slug: 'name', unique: true}
   }, {
     timestamps: true,
   })
